e1f58d4cf7fbb1250fb78f66104cd3e9a039d9eb
Oleksii D. Semilietov <spylik@gmail.com>
2016-08-05 16:35:25 +0300

    add macros to pub/1

--- include/erlroute.hrl
+++ include/erlroute.hrl
@@ -1,5 +1,6 @@
 % this record for keep internal message routing rules
 
+-define(PUB(Message), erlroute:pub(?MODULE, self(), ?LINE, mlibs:build_binary_key([?MODULE,?LINE]), Message).
 -define(PUB(Topic,Message), erlroute:pub(?MODULE, self(), ?LINE, Topic, Message).
 
 -record(msg_routes, {
