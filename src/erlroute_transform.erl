bcb6a38a3ecf39a3e41bfaeac6d1afd301383bc3
Oleksii D. Semilietov <spylik@gmail.com>
2016-09-12 14:47:10 +0300

    handle parse transform easy with pars_trans

--- src/erlroute_transform.erl
+++ src/erlroute_transform.erl
@@ -13,86 +13,21 @@
     -compile(export_all).
 -endif.
 
--export([parse_transform/2]).
-
--spec parse_transform(AST, Options) -> Result when
-    AST     ::  [erl_parse:abstract_form() | erl_parse:form_info()],
-    Options ::  [compile:option()],
-    Result  ::  [erl_parse:abstract_form() | erl_parse:form_info()].
-
-parse_transform(AST, _Options) ->
-%    io:format("~n~p~n", [Options]),
-walk_ast([],AST,[],[],[]).
-
-%-------------- walk_ast ----------------
-% from module we need keep module name in AtrAcc
-
-% parameterized module
-walk_ast(Acc, [{attribute, _, module, {Module, _ModArg}}=H|T], _Module, _Name, _Arity) ->
-    walk_ast([H|Acc], T, Module, [], []);
-% standart module
-walk_ast(Acc, [{attribute, _, module, Module}=H|T], _Module, _Name, _Arity) ->
-    walk_ast([H|Acc], T, Module, [], []);
-% dip only into functions
-walk_ast(Acc, [{function, Line, Name, Arity, Clauses}|T], Module, _Name, _Arity) ->
-    walk_ast([{function, Line, Name, Arity, walk_ast([], Clauses, Module, Name, Arity)}|Acc], T, Module, [], []);
-
-%--------- walk function clauses --------
-walk_ast(Acc, [{clause, Line, Arguments, Guards, Body}|T], Module, Name, Arity) ->
-    walk_ast([{clause, Line, Arguments, Guards, walk_clause_body([], Body, Module, Name, Arity)}|Acc], T, Module, Name, Arity);
-
-%------------- for the rest -------------
-walk_ast(Acc, [H|T], Module, Name, Arity) ->
-    walk_ast([H|Acc], T, Module, Name, Arity);
-
-walk_ast(Acc, [], _Module, _Name, _Arity) -> 
-    lists:reverse(Acc).
-
-%----------- walk functions body --------
-walk_clause_body(Acc, [H|T], Module, Name, Arity) ->
-    walk_clause_body([try_transform(H, Module,Name, Arity)|Acc], T, Module, Name, Arity);
-
-walk_clause_body(Acc, [], _Module, _Name, _Arity) ->
-    lists:reverse(Acc).
-
-%-------------- transform ---------------
-
-% transform 
-%   erlroute:pub(Message) 
-% to 
-%   erlroute:pub(Module, Pid, Line, Topic, Message)
-% and generate Topic as <<"Module.Line">>
-
-try_transform({call,Line,
+% erlroute:pub/1
+-define(pub1, 
+    {call,Line,
         {remote,Line,
             {atom,Line,erlroute},{atom,Line,pub}
         },
         [
             Msg
         ]
-    }, Module, _Name, _Arity) ->
-        Topic = lists:concat([Module,".",Line]),
-        Output = {call,Line,
-            {remote,Line,
-                {atom,Line,erlroute},{atom,Line,pub}
-            },
-            [
-                {atom,Line,Module},
-                {call, Line, {atom, Line ,self}, []},
-                {integer, Line, Line},
-                {bin, Line, [{bin_element,Line,{string,Line,Topic},default,default}]},
-                Msg 
-            ]
-        },
-%        io:format("output is ~p",[Output]),
-        Output;
+    }
+).
 
-
-% @doc transform 
-%   erlroute:pub(Topic, Message) 
-% to 
-%   erlroute:pub(Module, Pid, Line, Topic, Message, hybrid, EtsName)
-try_transform({call,Line,
+% erlroute:pub/2
+-define(pub2, 
+    {call,Line,
         {remote,Line,
             {atom,Line,erlroute},{atom,Line,pub}
         },
@@ -100,24 +35,57 @@ try_transform({call,Line,
             Topic,
             Msg
         ]
-    }, Module, _Name, _Arity) ->
-        EtsName = erlroute:generate_complete_routing_name(Module),
-        Output = {call,Line,
-            {remote,Line,
-                {atom,Line,erlroute},{atom,Line,pub}
-            },
-            [
-                {atom, Line,Module},
-                {call, Line, {atom, Line ,self}, []},
-                {integer, Line, Line},
-                Topic,
-                Msg,
-                {atom, Line, hybrid},
-                {atom, Line, EtsName}
-            ]
-        },
-%        io:format("output is ~p",[Output]),
-        Output;
+    }
+).
 
-try_transform(BodyElement, _Module, _Name, _Arity) ->
-    BodyElement.
+-export([parse_transform/2]).
+
+-spec parse_transform(AST, Options) -> Result when
+    AST     ::  [erl_parse:abstract_form() | erl_parse:form_info()],
+    Options ::  [compile:option()],
+    Result  ::  [erl_parse:abstract_form() | erl_parse:form_info()].
+
+parse_transform(Forms, _Options) ->
+    parse_trans:plain_transform(fun do_transform/1, Forms).
+
+do_transform(?pub1) ->
+    Module = test,
+    EtsName = erlroute:generate_complete_routing_name(Module),
+    Topic = lists:concat([Module,".",Line]),
+    Output = {call,Line,
+        {remote,Line,
+            {atom,Line,erlroute},{atom,Line,pub}
+        },
+        [
+            {atom,Line,Module},
+            {call, Line, {atom, Line ,self}, []},
+            {integer, Line, Line},
+            {bin, Line, [{bin_element,Line,{string,Line,Topic},default,default}]},
+            Msg,
+            {atom, Line, hybrid},
+            {atom, Line, EtsName}
+        ]
+    },
+    %io:format("output is ~p",[Output]),
+    Output;
+
+do_transform(?pub2) ->
+    Module = test,
+    EtsName = erlroute:generate_complete_routing_name(Module),
+    Output = {call,Line,
+        {remote,Line,
+            {atom,Line,erlroute},{atom,Line,pub}
+        },
+        [
+            {atom, Line,Module},
+            {call, Line, {atom, Line ,self}, []},
+            {integer, Line, Line},
+            Topic,
+            Msg,
+            {atom, Line, hybrid},
+            {atom, Line, EtsName}
+        ]
+    },
+    %io:format("output is ~p",[Output]),
+    Output;
+do_transform(_) -> continue.
