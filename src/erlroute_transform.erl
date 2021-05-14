799f307b0da02c94155e65c55c8c7984635c2fcb
Oleksii Semilietov <spylik@gmail.com>
2021-05-14 14:38:36 +0800

    support otp-24

--- src/erlroute_transform.erl
+++ src/erlroute_transform.erl
@@ -15,6 +15,8 @@
 
 -export([parse_transform/2]).
 
+-include("utils.hrl").
+
 -spec parse_transform(AST, Options) -> Result when
     AST     ::  [erl_parse:abstract_form() | erl_parse:form_info()],
     Options ::  [compile:option()],
@@ -26,38 +28,39 @@ parse_transform(Forms, _Options) ->
 
 % @doc transform erlroute:pub/1
 do_transform(
-    {call,Line,
-        {remote,Line,
-            {atom,Line,erlroute},{atom,Line,pub}
+    {call, _,
+        {remote, _,
+            {atom, Line,erlroute},{atom, _, pub}
         },
         [
             Msg
         ]
     }) ->
+    RealLine = realline(Line),
     Module = get(module),
     EtsName = erlroute:generate_complete_routing_name(Module),
-    Topic = lists:concat([Module,".",Line]),
-    Output = {call,Line,
-        {remote,Line,
-            {atom,Line,erlroute},{atom,Line,pub}
+    Topic = lists:concat([Module,".",RealLine]),
+    Output = {call,RealLine,
+        {remote,RealLine,
+            {atom,RealLine,erlroute},{atom,RealLine,pub}
         },
         [
-            {atom,Line,Module},
-            {call, Line, {atom, Line ,self}, []},
-            {integer, Line, Line},
-            {bin, Line, [{bin_element,Line,{string,Line,Topic},default,default}]},
+            {atom,RealLine,Module},
+            {call, RealLine, {atom, RealLine ,self}, []},
+            {integer, RealLine, RealLine},
+            {bin, RealLine, [{bin_element,RealLine,{string,RealLine,Topic},default,default}]},
             Msg,
-            {atom, Line, hybrid},
-            {atom, Line, EtsName}
+            {atom, RealLine, hybrid},
+            {atom, RealLine, EtsName}
         ]
     },
     Output;
 
 % @doc transform erlroute:pub/2
 do_transform(
-    {call,Line,
-        {remote,Line,
-            {atom,Line,erlroute},{atom,Line,pub}
+    {call, _,
+        {remote, _,
+            {atom, Line, erlroute},{atom, _, pub}
         },
         [
             Topic,
@@ -65,21 +68,25 @@ do_transform(
         ]
     }
 ) ->
+    RealLine = realline(Line),
     Module = get(module),
     EtsName = erlroute:generate_complete_routing_name(Module),
-    Output = {call,Line,
-        {remote,Line,
-            {atom,Line,erlroute},{atom,Line,pub}
+    {call,Line,
+        {remote,RealLine,
+            {atom,RealLine,erlroute},{atom,RealLine,pub}
         },
         [
-            {atom, Line,Module},
-            {call, Line, {atom, Line ,self}, []},
-            {integer, Line, Line},
+            {atom, RealLine,Module},
+            {call, RealLine, {atom, RealLine ,self}, []},
+            {integer, RealLine, RealLine},
             Topic,
             Msg,
-            {atom, Line, hybrid},
-            {atom, Line, EtsName}
+            {atom, RealLine, hybrid},
+            {atom, RealLine, EtsName}
         ]
-    },
-    Output;
-do_transform(_) -> continue.
+    };
+do_transform(SomethingAnother) ->
+    continue.
+
+realline({Line, _}) -> Line;
+realline(Line) when is_integer(Line) -> Line.
